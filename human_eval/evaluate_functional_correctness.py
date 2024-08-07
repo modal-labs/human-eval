58253ff06fdae86a5c214c41e41b3a74cdd4719f
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-08-07 19:49:50 +0800

    change type of k to list of ints

--- human_eval/evaluate_functional_correctness.py
+++ human_eval/evaluate_functional_correctness.py
@@ -1,5 +1,6 @@
 import fire
 import sys
+from typing import List
 
 from human_eval.data import HUMAN_EVAL
 from human_eval.evaluation import evaluate_functional_correctness
@@ -7,7 +8,7 @@ from human_eval.evaluation import evaluate_functional_correctness
 
 def entry_point(
     sample_file: str,
-    k: str = "1,10,100",
+    k: List[int] = [1, 10, 100],
     n_workers: int = 4,
     timeout: float = 3.0,
     problem_file: str = HUMAN_EVAL,
@@ -16,7 +17,6 @@ def entry_point(
     Evaluates the functional correctness of generated samples, and writes
     results to f"{sample_file}_results.jsonl.gz"
     """
-    k = list(map(int, k.split(",")))
     results = evaluate_functional_correctness(sample_file, k, n_workers, timeout, problem_file)
     print(results)
 
